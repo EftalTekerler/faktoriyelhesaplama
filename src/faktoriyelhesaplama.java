9610bee2f133fa260bb7bfc2345337f7b9163084
eftaltekerler <eftal.tekerler@yahoo.com>
2022-08-28 00:26:29 +0300

    Initial commit

---
+++ src/faktoriyelhesaplama.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class faktoriyelhesaplama {
+    public static void main(String[] args) {
+        // n! = 1 * 2 * 3 * 4 * ... * n
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Faktoriyel Sayısı");
+        int n = scan.nextInt();
+        int total = 1;
+        for (int i = 1; i <= n; i++){
+            total = total * i;
+       }
+        System.out.println(n+".Faktoriyel  " + total);
+    }
+}
